457c9259d9ab632e8fc47959f0b44081774ababb
Michal Moskal <michal@moskal.me>
2020-12-15 16:17:51 +0100

    Update schema to latest pxt

--- external/pxtpackage.d.ts
+++ external/pxtpackage.d.ts
@@ -1,6 +1,6 @@
 declare namespace pxt {
 
-    type CodeCardType = "file" | "example" | "codeExample" | "tutorial" | "side" | "template" | "package" | "hw" | "forumUrl";
+    type CodeCardType = "file" | "example" | "codeExample" | "tutorial" | "side" | "template" | "package" | "hw" | "forumUrl" | "forumExample" | "sharedExample" | "link";
     type CodeCardEditorType = "blocks" | "js" | "py";
 
     interface Map<T> {
@@ -24,17 +24,23 @@ declare namespace pxt {
         height: number;
     }
 
+    interface CodeCardAction {
+        url: string,
+        editor?: CodeCardEditorType;
+        cardType?: CodeCardType;
+    }
+
     /**
      * The schema for the pxt.json package files
      */
     interface PackageConfig {
         name: string;
         version?: string;
-        installedVersion?: string;
+        // installedVersion?: string; moved to Package class
         // url to icon -- support for built-in packages only
         icon?: string;
         // semver description for support target version
-        documentation?: string; // doc page to open when loading project
+        documentation?: string; // doc page to open when loading project, used by sidedocs
         targetVersions?: TargetVersions; // versions of the target/pxt the package was compiled against
         description?: string;
         dependencies: Map<string>;
@@ -43,16 +49,21 @@ declare namespace pxt {
         files: string[];
         simFiles?: string[];
         testFiles?: string[];
+        fileDependencies?: Map<string>; // exclude certain files if dependencies are not fulfilled
         preferredEditor?: string; // tsprj, blocksprj, pyprj
+        // languageRestriction?: pxt.editor.LanguageRestriction; // language restrictions that have been placed on the package
         testDependencies?: pxt.Map<string>;
         cppDependencies?: pxt.Map<string>;
         public?: boolean;
+        partial?: boolean; // true if project is not compileable on its own (eg base)
         binaryonly?: boolean;
         platformio?: PlatformIOConfig;
         compileServiceVariant?: string;
         palette?: string[];
+        paletteNames?: string[];
         screenSize?: Size;
         yotta?: YottaConfig;
+        codal?: CodalConfig;
         npmDependencies?: Map<string>;
         card?: CodeCard;
         additionalFilePath?: string;
@@ -73,6 +84,11 @@ declare namespace pxt {
         snippetBuilders?: SnippetConfig[];
         experimentalHw?: boolean;
         requiredCategories?: string[]; // ensure that those block categories are visible
+        supportedTargets?: string[]; // a hint about targets in which this extension is supported
+        firmwareUrl?: string; // link to documentation page about upgrading firmware
+        disablesVariants?: string[]; // don't build these variants, when this extension is enabled
+        utf8?: boolean; // force compilation with UTF8 enabled
+        disableTargetTemplateFiles?: boolean; // do not override target template files when commiting to github
     }
 
     interface PackageExtension {
@@ -90,6 +106,10 @@ declare namespace pxt {
     interface CompilationConfig {
         description: string;
         config: any;
+    }
+
+    interface CodalConfig {
+        libraries?: string[];
     }
 
     interface YottaConfig {
@@ -117,6 +137,7 @@ declare namespace pxt {
         labelClass?: string;
         tags?: string[]; // tags shown in home screen, colors specified in theme
         tabIndex?: number;
+        style?: string; // "card" | "item" | undefined;
 
         color?: string; // one of semantic ui colors
         description?: string;
@@ -125,7 +146,10 @@ declare namespace pxt {
         typeScript?: string;
         imageUrl?: string;
         largeImageUrl?: string;
+        videoUrl?: string;
         youTubeId?: string;
+        youTubePlaylistId?: string; // playlist this video belongs to
+        buttonLabel?: string;
         time?: number;
         url?: string;
         learnMoreUrl?: string;
@@ -134,13 +158,10 @@ declare namespace pxt {
         responsive?: boolean;
         cardType?: CodeCardType;
         editor?: CodeCardEditorType;
+        otherActions?: CodeCardAction[];
+        directOpen?: boolean; // skip the details view, directly do the card action
 
         header?: string;
-        any?: number;
-        hardware?: number;
-        software?: number;
-        blocks?: number;
-        javascript?: number;
 
         tutorialStep?: number;
         tutorialLength?: number;
@@ -164,6 +185,9 @@ declare namespace pxt {
         icon?: string; // URL (usually data-URI) for the icon
         namespace?: string; // used to construct id
         mimeType: string;
+        displayName?: string;
+        tilemapTile?: boolean;
+        tileset?: string[];
     }
 
     type SnippetOutputType = 'blocks'
@@ -179,7 +203,7 @@ declare namespace pxt {
         questions: SnippetQuestions[];
     }
 
-    type SnippetAnswerTypes = 'number' | 'text' | 'dropdown' | 'spriteEditor' | string; // TODO(jb) Should include custom answer types for number, enums, string, image
+    type SnippetAnswerTypes = 'number' | 'text' | 'variableName' | 'dropdown' | 'spriteEditor' | 'yesno' | string; // TODO(jb) Should include custom answer types for number, enums, string, image
 
     interface SnippetGoToOptions {
         question?: number;
@@ -223,9 +247,13 @@ declare namespace pxt {
         options: pxt.Map<string>;
     }
 
+    interface SnippetInputYesNoType {
+        type: "yesno";
+    }
+
     type SnippetQuestionInput = { label?: string; }
         & (SnippetInputAnswerSingular | SnippetInputAnswerPlural)
-        & (SnippetInputOtherType | SnippetInputNumberType | SnippetInputDropdownType)
+        & (SnippetInputOtherType | SnippetInputNumberType | SnippetInputDropdownType | SnippetInputYesNoType)
 
     interface SnippetValidateRegex {
         token: string;
@@ -241,6 +269,7 @@ declare namespace pxt {
     interface SnippetQuestions {
         title: string;
         output?: string;
+        outputConditionalOnAnswer?: string;
         errorMessage?: string;
         goto?: SnippetGoToOptions;
         inputs: SnippetQuestionInput[];
