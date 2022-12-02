fd4b2a939e156283d552d3023dbe87b7f17e4ccc
Richard Knoll <riknoll@users.noreply.github.com>
2022-12-02 15:39:22 -0800

    fix: add semicolons (#88)

--- packages/makecode-core/simloader/build.js
+++ packages/makecode-core/simloader/build.js
@@ -1,18 +1,18 @@
-const fs = require("fs")
-const root = "simloader/"
+const fs = require("fs");
+const root = "simloader/";
 
-let res = "export const simloaderFiles: Record<string, string> = {\n"
+let res = "export const simloaderFiles: Record<string, string> = {\n";
 
 function addFile(id, fn) {
-    const f = fs.readFileSync(root + fn, "utf8")
-    res += `"${id}": \`` + f.replace(/[\\`$]/g, x => "\\" + x) + "`,\n"
+    const f = fs.readFileSync(root + fn, "utf8");
+    res += `"${id}": \`` + f.replace(/[\\`$]/g, x => "\\" + x) + "`,\n";
 }
 
-addFile("loader.js", "built/loader.js")
-addFile("index.html", "index.html")
-addFile("custom.js", "custom.js")
+addFile("loader.js", "built/loader.js");
+addFile("index.html", "index.html");
+addFile("custom.js", "custom.js");
 
-res += "}\n"
-const outfn = "src/simloaderfiles.ts"
-console.log(`generate ${outfn}; ${res.length} bytes`)
-fs.writeFileSync(outfn, res)
+res += "}\n";
+const outfn = "src/simloaderfiles.ts";
+console.log(`generate ${outfn}; ${res.length} bytes`);
+fs.writeFileSync(outfn, res);
