3e485cdb418e7e66bbe6be668669f9ae8c7c5cfa
Richard Knoll <riknoll@users.noreply.github.com>
2023-01-10 14:21:53 -0800

    fix: linting (#91)

--- packages/makecode-core/simloader/loader.ts
+++ packages/makecode-core/simloader/loader.ts
@@ -17,7 +17,7 @@ function addSimMessageHandler(
         channel: channel,
         init: init,
         handler: handler,
-    }
+    };
 }
 
 
@@ -25,32 +25,32 @@ const pendingMessages: {[index: string]: (result: string) => void} = {};
 let nextMessageId = 0;
 
 function makeCodeRun(options) {
-    let code = ""
-    let code0 = ""
-    let isReady = false
-    let simState = {}
-    let simStateChanged = false
-    let started = false
-    let meta = undefined
-    let boardDefinition = undefined
-    let simOrigin = undefined
-    const selfId = options.selfId || "pxt" + Math.random()
-    const tool = options.tool
-    const isLocalHost = /^(localhost|127\.0\.0\.1)(:|$)/i.test(window.location.host)
+    let code = "";
+    let code0 = "";
+    let isReady = false;
+    let simState = {};
+    let simStateChanged = false;
+    let started = false;
+    let meta = undefined;
+    let boardDefinition = undefined;
+    let simOrigin = undefined;
+    const selfId = options.selfId || "pxt" + Math.random();
+    const tool = options.tool;
+    const isLocalHost = /^(localhost|127\.0\.0\.1)(:|$)/i.test(window.location.host);
 
     // hide scrollbar
-    window.scrollTo(0, 1)
+    window.scrollTo(0, 1);
 
-    const lckey = "pxt_frameid_" + tool
+    const lckey = "pxt_frameid_" + tool;
     if (!localStorage[lckey])
-        localStorage[lckey] = "x" + Math.round(Math.random() * 2147483647)
-    let frameid = localStorage[lckey]
+        localStorage[lckey] = "x" + Math.round(Math.random() * 2147483647);
+    let frameid = localStorage[lckey];
 
     // init runtime
-    initSimState()
-    startCode()
+    initSimState();
+    startCode();
     if (isLocalHost)
-        autoReload()
+        autoReload();
 
     function fetchSourceCode() {
         if (options.usePostMessage) {
@@ -59,7 +59,7 @@ function makeCodeRun(options) {
             });
         }
         return fetch(options.js)
-            .then(resp => resp.status == 200 ? resp.text() : undefined)
+            .then(resp => resp.status == 200 ? resp.text() : undefined);
     }
 
     // helpers
@@ -75,43 +75,43 @@ function makeCodeRun(options) {
     function startCode() {
         fetchSourceCode()
             .then(c => {
-                if (!c) return
-                code0 = code = c
+                if (!c) return;
+                code0 = code = c;
                 // find metadata
                 code.replace(/^\/\/\s+meta=([^\n]+)\n/m, function (m, metasrc) {
-                    meta = JSON.parse(metasrc)
-                    return ""
-                })
+                    meta = JSON.parse(metasrc);
+                    return "";
+                });
                 code.replace(
                     /^\/\/\s+boardDefinition=([^\n]+)\n/m,
                     function (m, metasrc) {
-                        boardDefinition = JSON.parse(metasrc)
-                        return ""
+                        boardDefinition = JSON.parse(metasrc);
+                        return "";
                     }
-                )
-                document.body.dataset.version = meta?.version
+                );
+                document.body.dataset.version = meta?.version;
                 // force local sim
                 if (isLocalHost)
-                    meta.simUrl = window.location.protocol + "//" + window.location.host + `/sim.html${window.location.search || ""}`
+                    meta.simUrl = window.location.protocol + "//" + window.location.host + `/sim.html${window.location.search || ""}`;
 
                 const ap = document.getElementById("download-a") as HTMLAnchorElement
                 if (meta.version && ap && ap.download)
-                    ap.download = ap.download.replace(/VERSION/, meta.version)
+                    ap.download = ap.download.replace(/VERSION/, meta.version);
 
                 // load simulator with correct version
                 document
                     .getElementById("simframe")
-                    .setAttribute("src", meta.simUrl + "#" + frameid)
-                let m = /^https?:\/\/[^\/]+/.exec(meta.simUrl)
-                simOrigin = m[0]
-                initFullScreen()
+                    .setAttribute("src", meta.simUrl + "#" + frameid);
+                let m = /^https?:\/\/[^\/]+/.exec(meta.simUrl);
+                simOrigin = m[0];
+                initFullScreen();
             })
     }
 
     function startSim() {
-        if (!code || !isReady || started) return
-        setState("run")
-        started = true
+        if (!code || !isReady || started) return;
+        setState("run");
+        started = true;
         const runMsg = {
             type: "run",
             parts: [],
@@ -129,29 +129,29 @@ function makeCodeRun(options) {
                 player: "",
             },
             id: "green-" + Math.random(),
-        }
-        postMessage(runMsg)
+        };
+        postMessage(runMsg);
     }
 
     function stopSim() {
-        setState("stopped")
+        setState("stopped");
         postMessage({
             type: "stop",
-        })
-        started = false
+        });
+        started = false;
     }
 
     window.addEventListener(
         "message",
         function (ev) {
-            let d = ev.data
+            let d = ev.data;
             // console.debug(ev.origin, d)
             if (ev.origin == simOrigin) {
                 if (d.type == "ready") {
-                    let loader = document.getElementById("loader")
-                    if (loader) loader.remove()
-                    isReady = true
-                    startSim()
+                    let loader = document.getElementById("loader");
+                    if (loader) loader.remove();
+                    isReady = true;
+                    startSim();
                 } else if (d.type == "simulator") {
                     switch (d.command) {
                         case "restart":
@@ -164,21 +164,21 @@ function makeCodeRun(options) {
                             break
                         case "setstate":
                             if (d.stateValue === null)
-                                delete simState[d.stateKey]
-                            else simState[d.stateKey] = d.stateValue
-                            simStateChanged = true
+                                delete simState[d.stateKey];
+                            else simState[d.stateKey] = d.stateValue;
+                            simStateChanged = true;
                             break
                     }
                 } else if (d.type === "debugger") {
                     // console.log("dbg", d)
-                    let brk = d
-                    let stackTrace = brk.exceptionMessage + "\n"
+                    let brk = d;
+                    let stackTrace = brk.exceptionMessage + "\n";
                     for (let s of brk.stackframes) {
-                        let fi = s.funcInfo
+                        let fi = s.funcInfo;
                         stackTrace += `   at ${fi.functionName} (${fi.fileName
-                            }:${fi.line + 1}:${fi.column + 1})\n`
+                            }:${fi.line + 1}:${fi.column + 1})\n`;
                     }
-                    if (brk.exceptionMessage) console.error(stackTrace)
+                    if (brk.exceptionMessage) console.error(stackTrace);
                     postMessageToParentAsync(d);
                 } else if (d.type === "messagepacket" && d.channel) {
                     if (
@@ -186,16 +186,16 @@ function makeCodeRun(options) {
                         d.broadcast &&
                         window.parent != window
                     ) {
-                        d.sender = selfId
-                        window.parent.postMessage(d, "*")
+                        d.sender = selfId;
+                        window.parent.postMessage(d, "*");
                     }
                     const ch = channelHandlers[d.channel]
                     if (ch) {
                         try {
-                            ch.handler(d.data)
+                            ch.handler(d.data);
                         } catch (e) {
-                            console.log(`invalid simmessage`)
-                            console.log(e)
+                            console.log(`invalid simmessage`);
+                            console.log(e);
                         }
                     }
                 }
@@ -208,9 +208,9 @@ function makeCodeRun(options) {
                     d.channel == "jacdac" &&
                     d.sender != selfId
                 ) {
-                    postMessage(d)
+                    postMessage(d);
                 } else if (d.type == "reload") {
-                    window.location.reload()
+                    window.location.reload();
                 }
                 else if (d.type == "fetch-js") {
                     pendingMessages[d.id](d.text);
@@ -219,7 +219,7 @@ function makeCodeRun(options) {
             }
         },
         false
-    )
+    );
 
     // initialize simmessages
     Object.keys(channelHandlers)
@@ -230,38 +230,31 @@ function makeCodeRun(options) {
                 type: "messagepacket",
                 channel: ch.channel,
                 data: msg
-            })
+            });
             ch.init({ send });
         })
 
-    // helpers
-    function uint8ArrayToString(input) {
-        let len = input.length
-        let res = ""
-        for (let i = 0; i < len; ++i) res += String.fromCharCode(input[i])
-        return res
-    }
-
     function setState(st) {
-        let r = document.getElementById("root")
-        if (r) r.setAttribute("data-state", st)
+        let r = document.getElementById("root");
+        if (r) r.setAttribute("data-state", st);
     }
 
     function postMessage(msg) {
         const frame = document.getElementById("simframe") as HTMLIFrameElement
-        if (meta && frame) frame.contentWindow.postMessage(msg, meta.simUrl)
+        if (meta && frame) frame.contentWindow.postMessage(msg, meta.simUrl);
     }
 
     function initSimState() {
         try {
-            simState = JSON.parse(localStorage["pxt_simstate"])
+            simState = JSON.parse(localStorage["pxt_simstate"]);
         } catch (e) {
-            simState = {}
+            simState = {};
         }
         setInterval(function () {
-            if (simStateChanged)
-                localStorage["pxt_simstate"] = JSON.stringify(simState)
-            simStateChanged = false
+            if (simStateChanged) {
+                localStorage["pxt_simstate"] = JSON.stringify(simState);
+            }
+            simStateChanged = false;
         }, 200)
     }
 
