13ce46b3ad95526527e84212acb74acfada65b88
Richard Knoll <riknoll@microsoft.com>
2024-07-17 10:25:21 -0700

    Remove yarn and normalize package scripts

--- packages/makecode-core/simloader/build.js
+++ packages/makecode-core/simloader/build.js
@@ -1,18 +1,37 @@
 const fs = require("fs");
 const root = "simloader/";
 
-let res = "export const simloaderFiles: Record<string, string> = {\n";
+const outFile = "src/simloaderfiles.ts";
 
-function addFile(id, fn) {
-    const f = fs.readFileSync(root + fn, "utf8");
-    res += `"${id}": \`` + f.replace(/[\\`$]/g, x => "\\" + x) + "`,\n";
+if (process.argv[2] === "clean") {
+    clean()
+}
+else {
+    build();
 }
 
-addFile("loader.js", "built/loader.js");
-addFile("index.html", "index.html");
-addFile("custom.js", "custom.js");
+function build() {
+    let res = "export const simloaderFiles: Record<string, string> = {\n";
+    function addFile(id, fn) {
+        const f = fs.readFileSync(root + fn, "utf8");
+        res += `"${id}": \`` + f.replace(/[\\`$]/g, x => "\\" + x) + "`,\n";
+    }
 
-res += "}\n";
-const outfn = "src/simloaderfiles.ts";
-console.log(`generate ${outfn}; ${res.length} bytes`);
-fs.writeFileSync(outfn, res);
+    addFile("loader.js", "built/loader.js");
+    addFile("index.html", "index.html");
+    addFile("custom.js", "custom.js");
+
+    res += "}\n";
+    console.log(`generate ${outFile}; ${res.length} bytes`);
+    fs.writeFileSync(outFile, res);
+}
+
+function clean() {
+    try {
+        fs.unlinkSync(outFile);
+    }
+    catch (e) {
+        // ignore
+    }
+}
+
